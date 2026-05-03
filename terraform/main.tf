c46cd6f4318187eadf81040fafdc4b55813f57f8
varunudata <varunudata@gmail.com>
2026-05-04 00:15:20 +0530

    added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "zenvy-app-bucket-${random_id.bucket_suffix.hex}"
+
+  tags = {
+    Name        = "zenvy-app-bucket"
+    Environment = "Dev"
+  }
+}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_bucket_encryption" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block all public access
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
