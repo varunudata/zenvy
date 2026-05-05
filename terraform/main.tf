6c08916a344bdf1ef8a6497ef8609f363280861e
varunudata <varunudata@gmail.com>
2026-05-05 22:03:03 +0530

    chore: fully restrict public access to S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 
